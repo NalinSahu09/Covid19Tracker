784019e2d52e062c43daf5eb1dff9e3e7246c667
NalinSahu09 <nalinsahu09@gmail.com>
2022-02-07 10:22:07 +0530

    splash, app-component, menu,header, covid19 guidlines and footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,42 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactUsComponent } from './main-screen/contact-us/contact-us.component';
+import { CountriesComponent } from './main-screen/countries/countries.component';
+import { Covid19GuidelinesComponent } from './main-screen/covid19-guidelines/covid19-guidelines.component';
+import { DashboardComponent } from './main-screen/dashboard/dashboard.component';
+import { FaqComponent } from './main-screen/faq/faq.component';
+import { IndiaComponent } from './main-screen/india/india.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', 
+    component:DashboardComponent
+  },
+  {
+    path:'dashboard', 
+    component:DashboardComponent
+  },
+  {
+    path:'countries',
+    component:CountriesComponent
+  },
+  {
+    path:'india',
+    component:IndiaComponent
+  },
+  {
+    path:'faq',
+    component:FaqComponent
+  },
+  {
+    path:'contact-us',
+    component:ContactUsComponent
+  },
+  {
+    path:'covid19-guidelines',
+    component:Covid19GuidelinesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/splash-screen/splash-screen.component.ts
+++ src/app/splash-screen/splash-screen.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-splash-screen',
   templateUrl: './splash-screen.component.html',
-  styleUrls: ['./splash-screen.component.scss']
+  styleUrls: ['./splash-screen.component.scss'],
+  encapsulation : ViewEncapsulation.None
 })
 export class SplashScreenComponent implements OnInit {
+
+  windowWidth : String = "";
+  showSplash : boolean = true;
 
   constructor() { }
 
   ngOnInit(): void {
+    setTimeout( ()=>{
+      this.windowWidth = '-' + window.innerWidth + 'px';
+
+      setTimeout( ()=>{
+        this.showSplash = !this.showSplash
+      }, 2400);
+    }, 3000);
   }
 
 }
